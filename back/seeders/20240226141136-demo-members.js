23558d19268ca080b40745c78ef3aedfe89192f5
Judit Calero <judit.calero84@gmail.com>
2024-03-18 14:24:24 +0100

    seeders for members and workers fixed so we can test things in the front nicely

--- back/seeders/20240226141136-demo-members.js
+++ back/seeders/20240226141136-demo-members.js
@@ -6,16 +6,6 @@ const { v4: uuidv4 } = require('uuid')
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
-   
 
     await queryInterface.bulkInsert('Members', [
       {
@@ -45,7 +35,63 @@ module.exports = {
         memberType: 'entity',
         createdAt: new Date(),
         updatedAt: new Date()
-      },])
+      },
+      {
+        id: "d9f281b8-d231-464d-a119-62621af6b33f",
+        commercialName1: 'Casa Ametlla',
+        email: 'info@casaametlla.cat',
+        officialId: "R44455566",
+        address: "POL. IND. ELS CRAPITOS, CARRER TRAMUNTANA 110, ARTÉS",
+        city: "ARTÉS",
+        postcode: "08777",
+        country: "SPAIN",
+        phoneNumber: "935556677",
+        memberType: 'entity',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        id: "d3c92e9c-b840-43b6-b84e-e7a84f8ea595",
+        commercialName1: 'Super Talló SA',
+        email: 'abacusonline@abacus.coop',
+        officialId: "A08969420",
+        address: "PLAZA TRIOMF, 24",
+        city: "TERRASSA",
+        postcode: "08225",
+        country: "SPAIN",
+        phoneNumber: "937357722",
+        memberType: 'entity',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        id: "a115a827-542f-4417-b8a8-f29c6ea790ef",
+        commercialName1: 'Universitat Autònoma de Barcelona',
+        email: 'info@uab.cat',
+        officialId: "Q0818002H",
+        address: "Campus de la UAB",
+        city: "BELLATERRA",
+        postcode: "08193",
+        country: "SPAIN",
+        phoneNumber: "937357722",
+        memberType: 'entity',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        id: "58d7c496-f5d6-402e-858e-c50db7eafcf8",
+        commercialName1: 'ASSOCIACIO SYNUSIA',
+        email: 'info@sinusia.cat',
+        officialId: "G65738965",
+        address: "Carrer Sant Gaietà, 73",
+        city: "TERRASSA",
+        postcode: "08221",
+        country: "SPAIN",
+        phoneNumber: "937357722",
+        memberType: 'entity',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      }])
   },
 
   async down (queryInterface, Sequelize) {
---
+++ back/seeders/20240301141137-demo-workers.js
@@ -0,0 +1,60 @@
+'use strict';
+
+const { v4: uuidv4 } = require('uuid')
+
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+   
+
+    await queryInterface.bulkInsert('Workers', [
+      {
+        id: "74114925-269f-4f44-89bd-6d9259847489",
+        firstname: "Judit",
+        lastname2: "Timoneda",
+        lastname1: "Calero",
+        pronouns: "she/her",
+        role: "IT developer",
+        officialId: "46772100J",
+        email: "judit.calero84@gmail.com",
+        username: "Judit",
+        password: "$2b$10$IF2j7SrHWntEfa7fOs4IQ.EHrp1f5YHsbEB39tluHuBn22WylfzkO",
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      { 
+        id: "94b5c901-57a1-41cf-a81c-45a3b3fcb3dc",
+        firstname: "Iris",
+        lastname2: "Natassie Georges Lulu",
+        lastname1: "Huard",
+        pronouns: "she/her",
+        role: "IT developer",
+        officialId: "46772100J",
+        email: "someirismail@gmail.com",
+        username: "Iris",
+        password: "$2b$10$qMB7jIbA.9jGKquBC5Ki7O25Pnmf2ePfPsu2BpKWlxN6nXxxkPcne",
+        createdAt: new Date(),
+        updatedAt: new Date()
+      }])
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+    await queryInterface.bulkDelete('Transactions', null, {});
+  }
+};
