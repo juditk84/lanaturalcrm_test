569d28bbd4064fbc9669e582f2cf77398ee8896e
luluconcha <iris.mihu@gmail.com>
2024-04-02 11:56:55 +0200

    detalls seeders

--- back/seeders/20240226141135-demo-workers.js
+++ back/seeders/20240226141135-demo-workers.js
@@ -49,12 +49,7 @@ module.exports = {
   },
 
   async down (queryInterface, Sequelize) {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
+
     await queryInterface.bulkDelete('Transactions', null, {});
   }
 };
