a201050d52b1f816cd36e3f07400cc7822f5f862
Judit Calero <judit.calero84@gmail.com>
2024-05-23 11:59:19 +0200

    stuck in the tasks in calendar grid component. I don't know how to tackle it yet

--- front/src/views/Tasques/GridTasksCalendar.vue
+++ front/src/views/Tasques/GridTasksCalendar.vue
@@ -1,14 +1,12 @@
 <script setup>
     import LayoutAuthenticated from '@/layouts/LayoutAuthenticated.vue'
-    import { ref } from 'vue'
-
+    import { computed, ref } from 'vue'
+    import calendarAPIprovisional from '@/calendarAPIprovisional.js'
     const arrayOfDraggables = ref(['red', 'green', "pink", "cyan"])
-
+    
     function handleDragStart(e) {
         e.target.style.opacity = '0.4';
         e.dataTransfer.setData("text/plain", e.target.id)
-        console.log(e.target.id)
-        // console.log(e.dataTransfer.getData("text/plain"))
     }
 
     function handleDragEnd(e) {
@@ -16,11 +14,14 @@
     }
 
     function handleDrop(e) {
-        // s'agafa l'element del droppable, es fica en el dataTransfer, es getteja en el drop event i es fa servir l'id(index) en el
-        // e.target del droppable i es fa un recolocaçao de l'arrayOfDraggables.
+        // storing these for readability:
         const indexOfItemMoved = e.dataTransfer.getData("text/plain")
         const indexOfDroppable = e.target.id
-        const itemMoved = arrayOfDraggables.value.splice(indexOfItemMoved, 1)[0]
+
+        // splice() returns an array, since it's always gonna be 1 object moved, I grab the [0] index in the array.
+        const itemMoved = arrayOfDraggables.value.splice(indexOfItemMoved, 1)[0] 
+
+        // delete and add operation:
         arrayOfDraggables.value.splice(indexOfDroppable, 0, itemMoved)
     }
 
@@ -28,23 +29,46 @@
         e.preventDefault()
     }
 
-    
 
 
 
+    const today = ref(new Date())
+    const todayDay = today.value.getDate()
+    const todayMonth = today.value.getMonth()
+
+    // volem un display que tingui
+    // |                                        year                           |                                        year                            |
+    // |month|month|month|month|month|month|month|month|month|month|month|month|month|month|month|month|month|month|month|month|month|month|month|month
+    //  days  days  days  days  etc
 </script>
 
 <template>
-    <LayoutAuthenticated>
-        <div>hola</div>
-        <div class="outline self-center grid grid-cols-4 px-96">
+<div class="grid grid-cols-12 max-w-none overflow-auto">
+    <div>1</div>
+    <div>1</div>
+    <div>1</div>
+    <div>1</div>
+    <div>1</div>
+    <div>1</div>
+    <div>1</div>
+    <div>1</div>
+    <div>1</div>
+    <div>1</div>
+    <div>1</div>
+    <div>1</div>
+    <div>1</div>
+    <div>1</div>
+    <div>1</div>
+</div>
+        
+        <div class="outline self-center grid grid-cols-4">
             <div v-for="cosa, index of arrayOfDraggables" 
                  :id="index" @dragstart="(e) => handleDragStart(e)"
-                 @dragend="(e) => handleDragEnd(e)"
-                 @dragover="(e) => allowDrag(e)"
-                 @drop="(e) => handleDrop(e)"
+                 @dragend=  "(e) => handleDragEnd(e)"
+                 @dragover= "(e) => allowDrag(e)"
+                 @drop=     "(e) => handleDrop(e)"
                  draggable="true" 
                  :class="`outline m-1 bg-${cosa}-400`">{{ cosa }}</div>
         </div>
-    </LayoutAuthenticated>
+
 </template>
